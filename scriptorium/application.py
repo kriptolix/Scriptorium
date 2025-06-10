7a296f1492b0ab71c26ba29e5abffdc7b0929e5b
Christophe Gueret <christophe.gueret@gmail.com>
2025-06-10 08:49:34 +0100

    Added code to embed and start LanguageTool
    Will try to connect to a server running on the expected port. This makes it possible to connect, for instance, with an instance managed by Eloquent

---
+++ scriptorium/application.py
@@ -0,0 +1,129 @@
+# application.py
+#
+# Copyright 2025 Christophe Guéret
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+import sys
+from scriptorium.widgets.multiline_entry_row import MultiLineEntryRow
+from gi.repository import Gio, Adw, GLib, GObject
+from .window import ScrptWindow
+from .language_tool import LanguageTool
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(name)-40s: %(levelname)-8s %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+
+class ScriptoriumApplication(Adw.Application):
+    """The main application singleton class."""
+
+    # The LanguageTool wrapper
+    language_tool = GObject.Property(type=LanguageTool)
+
+    def __init__(self):
+        super().__init__(application_id='io.github.cgueret.Scriptorium',
+                         flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
+        self.create_action('quit', lambda *_: self.quit(), ['<primary>q'])
+        self.create_action('about', self.on_about_action)
+        self.create_action('preferences', self.on_preferences_action)
+
+        self.settings = Gio.Settings(
+            schema_id="io.github.cgueret.Scriptorium"
+        )
+        style_variant_action = self.settings.create_action("style-variant")
+        self.add_action(style_variant_action)
+
+        self.settings.connect(
+            "changed::style-variant",
+            self.change_color_scheme
+        )
+
+        # Get the current color scheme
+        current_value = self.settings.get_string("style-variant")
+        style_variant_action.activate(GLib.Variant('s', current_value))
+
+        # Force loading MultiLineEntryRow, otherwise it is not recognized in Builder
+        dummy = MultiLineEntryRow()
+        del dummy
+
+        self.connect("startup", self.on_startup)
+
+    def change_color_scheme(self, _settings, value):
+        """Handle a change in the select color scheme setting."""
+        color_scheme = self.settings.get_string(value)
+
+        style_manager = Adw.StyleManager.get_default()
+        if color_scheme == 'dark':
+            style_manager.set_color_scheme(Adw.ColorScheme.FORCE_DARK)
+        elif color_scheme == 'light':
+            style_manager.set_color_scheme(Adw.ColorScheme.FORCE_LIGHT)
+        elif color_scheme == 'default':
+            style_manager.set_color_scheme(Adw.ColorScheme.DEFAULT)
+
+    def do_activate(self):
+        """Called when the application is activated.
+
+        We raise the application's main window, creating it if
+        necessary.
+        """
+        window = self.props.active_window
+        if not window:
+            window = ScrptWindow(application=self)
+        window.present()
+
+
+    def on_about_action(self, *args):
+        """Callback for the app.about action."""
+        about = Adw.AboutDialog(application_name='Scriptorium',
+                                application_icon='io.github.cgueret.Scriptorium',
+                                developer_name='Christophe Guéret',
+                                version='0.2.1',
+                                website='https://github.com/cgueret/Scriptorium',
+                                developers=['Christophe Guéret'],
+                                copyright='© 2025 Christophe Guéret')
+        # Translators: Replace "translator-credits" with your name/username, and optionally an email or URL.
+        #about.set_translator_credits(_('translator-credits'))
+        about.present(self.props.active_window)
+
+    def on_preferences_action(self, widget, _):
+        """Callback for the app.preferences action."""
+        logger.info('app.preferences action activated')
+
+    def create_action(self, name, callback, shortcuts=None):
+        """Add an application action.
+
+        Args:
+            name: the name of the action
+            callback: the function to be called when the action is
+              activated
+            shortcuts: an optional list of accelerators
+        """
+        action = Gio.SimpleAction.new(name, None)
+        action.connect("activate", callback)
+        self.add_action(action)
+        if shortcuts:
+            self.set_accels_for_action(f"app.{name}", shortcuts)
+
+    def on_startup(self, _application):
+        # Instantiate our language tool interface
+        self.language_tool = LanguageTool()
+
+        self.language_tool.check("Hello worlds!", "en-GB")
+
+
---
+++ scriptorium/language_tool.py
@@ -0,0 +1,114 @@
+# language_tool.py
+#
+# Copyright 2025 Unknown
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# Code inspired from https://github.com/sonnyp/Eloquent/blob/main/src/languagetool.js
+from gi.repository import Gio, GObject, Soup, GLib
+import logging
+
+logger = logging.getLogger(__name__)
+
+SEND_PING_TIMEOUT_SECONDS = 2
+
+
+class LanguageTool(GObject.Object):
+
+    # This is True when we could connect to Language Tool, False otherwise
+    server_is_alive = GObject.Property(type=bool, default=False)
+
+    def __init__(self):
+        super().__init__()
+
+        self._session = Soup.Session()
+
+        self._proc_language_tool = None
+
+        GLib.timeout_add_seconds(SEND_PING_TIMEOUT_SECONDS, self.send_ping)
+
+    def send_ping(self):
+        message = Soup.Message.new("GET", "http://localhost:8081")
+        self._session.send_and_read_async(
+            message, GObject.PRIORITY_LOW, None, self.handle_ping_reply
+        )
+        return True
+
+    def handle_ping_reply(self, session, result):
+        try:
+            message = session.send_and_read_finish(result)
+            if "LanguageTool API" in message.get_data().decode():
+                self.server_is_alive = True
+        except GLib.GError:
+            # We could not connect
+            self.server_is_alive = False
+
+            # Let's try to start it
+            self._start_server()
+
+    def _start_server(self):
+        # Only try once!
+        if self._proc_language_tool is not None:
+            return
+
+        logging.info("Starting LanguageTool server")
+
+        # Start the subprocess
+        self._proc_language_tool = Gio.Subprocess.new(
+            [
+                "java",
+                "-cp",
+                "/app/LanguageTool/languagetool-server.jar",
+                "org.languagetool.server.HTTPServer",
+                "--allow-origin",
+                "--public",
+                "--port",
+                "8081",
+            ],
+            Gio.SubprocessFlags.INHERIT_FDS | Gio.SubprocessFlags.STDOUT_PIPE,
+        )
+
+        # TODO Replace this code by an async to diplay the logs from LanguageTool
+        # Wait until we can read "Server started"
+        #stdout_stream = Gio.DataInputStream(
+        #    base_stream=self._proc_language_tool.get_stdout_pipe(),
+        #    close_base_stream=True,
+        #)
+        #started = False
+        #while not started:
+        #    message, length = stdout_stream.read_line()
+        #    logger.info(message.decode())
+        #    started = "Server started" in message.decode()
+
+    def check(self, text: str, language: str):
+        if not self.server_is_alive:
+            return
+
+        encoded = Soup.form_encode_hash({
+            "text": text,
+            "language": language,
+        })
+
+        message = Soup.Message.new_from_encoded_form(
+            method = "POST",
+            uri_string = "http://localhost:8081/v2/check",
+            encoded_form = encoded,
+        )
+
+        response = self._session.send_and_read(msg=message, cancellable=None)
+
+        logger.info(response)
+        
