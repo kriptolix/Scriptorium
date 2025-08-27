58412919d34a2b5fae384deda8b1cab941d3bdfc
Christophe Gueret <christophe.gueret@gmail.com>
2025-08-27 17:55:12 +0100

    Plan: Fix adding scenes

--- scriptorium/views/plan/editor_scenes.py
+++ scriptorium/views/plan/editor_scenes.py
@@ -71,7 +71,10 @@ class ScrptScenesPanel(Adw.NavigationPage):
             response = dialog.choose_finish(task)
             if response == "add":
                 logger.info(f"Add scene {dialog.title}: {dialog.synopsis}")
-                self.editor.project.create_resource(Scene, dialog.title, dialog.synopsis)
+                resource = self._editor.project.create_resource(
+                    Scene, dialog.title, dialog.synopsis
+                )
+                self._editor.project.manuscript.add_resource(resource)
 
         dialog = ScrptAddDialog("scene")
         dialog.choose(self, None, handle_response)
